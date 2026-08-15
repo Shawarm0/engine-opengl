e0fda426a12a3b2c973f18d1dcce48dc82e7f959
Lovin Sharma <130526452+Shawarm0@users.noreply.github.com>
2026-08-15 22:52:44 +0100

    Boilerplate 1: GLFW initialisation and set window hints

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include <stdio.h>
 
+int main(void) {
+
+	// OpenGL context initialisation failed
+	if (!glfwInit()) {
+		fprintf(stderr, "glfwInit failed\n");
+		return 1;
+	}
 
 
-int main(int argc, char* argv[]) {
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+#endif
+
 	printf("Hello World\n");
 	return 0;
 }
